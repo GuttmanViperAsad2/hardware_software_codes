80379307b02fe9b97d5be1fe27d8857d11867702
GuttmanViperAsad <ASAD.FEVRIER16@guttmanmail.cuny.edu>
2025-01-14 11:11:47 -0500

    first commit

---
+++ ADF_project_01.py
@@ -0,0 +1,19 @@
+def main():
+    name = "Professor Nedd"
+    print("Hello user-->")
+    print("Hello! Welcome to Intor of Hardware and Software")
+    print()
+    print("In this class we will be learning how to master Software and Hardware!")
+    print()
+    print("What is your name student and what college are you attending?")
+    print()
+    print("What is the High School you graduated from?")
+    print()
+    print("Which Institution do you think is the most fun?")
+    print()
+    print("What is it do you seek in this class? {}?".format(name))
+    answer = input()
+    print("I seek a promising furture in computers! You said {}?".format(answer))
+    print("Let’s start learning some Python today")
+if __name__ == "__main__":
+    main()
